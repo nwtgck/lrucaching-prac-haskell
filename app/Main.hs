af2511576702c00bd574b9ddc4ecc4009ec02c55
Ryo Ota <nwtgck@gmail.com>
2018-04-07 12:19:47 +0900

    [add] Add simple caching practice

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,32 @@
 module Main where
 
--- TODO: Implement
+import qualified Data.LruCache.IO as LruCache.IO
+import Control.Concurrent
+
+slowMul10 :: (LruCache.IO.LruHandle Int [Int]) -> Int -> IO [Int]
+slowMul10 cache num = LruCache.IO.cached cache num $ do
+  threadDelay (3 * 1000 * 1000) 
+  return (replicate 100000 num)
+
+  -- TODO: Implement
 main :: IO ()
 main = do
-  putStrLn "hello, world from stack executable template"
+  putStrLn "Start!"
+  cache1 <- LruCache.IO.newLruHandle 1000 -- Capacity is number of keys
+  res <- slowMul10 cache1 12
+  -- print res
+  putStrLn "Done12"
+  res <- slowMul10 cache1 12
+  -- print res
+  putStrLn "Done12"
+  
+  res <- slowMul10 cache1 33
+  putStrLn "Done33"  
+  -- print res
+  res <- slowMul10 cache1 33
+  -- print res
+  putStrLn "Done33"
+  res <- slowMul10 cache1 12
+  -- print res
+  putStrLn "Done12"
+  
